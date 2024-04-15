bd8a3a90154af7ecfce8638e966f328ae346b292
Dalibor Drevojanek <drevojanekdalibor@gmail.com>
2024-04-15 22:58:15 +0200

    Chaper 4

--- src/Html/Internal.hs
+++ src/Html/Internal.hs
@@ -41,9 +41,9 @@ ol_ :: [Structure] -> Structure
 ol_ =
     Structure . el "ol" . concatMap (el "li" . getStructureString)
 
-append_ :: Structure -> Structure -> Structure
-append_ c1 c2 =
-  Structure (getStructureString c1 <> getStructureString c2)
+instance Semigroup Structure where
+  (<>) c1 c2 =
+    Structure (getStructureString c1 <> getStructureString c2)
 
 -- * Render
 
